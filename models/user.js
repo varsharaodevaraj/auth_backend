41cc7fd5fabcb3171ba54fb6ce866435cf863294
Varsha Rao <varsharao@Varshas-MacBook-Air.local>
2025-08-10 23:42:28 +0530

    commit

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -31,9 +32,10 @@ const userSchema = new mongoose.Schema({
 // })
 
 userSchema.pre('save',async function (next){
-    const salt = await b
-    this.password = await bcrypt(this.password,);
-})
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+});
 
 const User = mongoose.model('user',userSchema);
 
